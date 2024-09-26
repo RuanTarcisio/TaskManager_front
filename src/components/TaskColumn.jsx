18d597a1304f033bccf39d42a84322eb941b4b76
Ruan Tarcisio <ruantarciisio@gmail.com>
2024-09-26 20:07:39 -0300

    subindo para o git, pendente de ajustes.

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import Todo from "../assets/direct-hit.png"
+import React from "react";
 
+import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
-const TaskColumn = () => {
-  return (
-    <section>
-        <h2><img src={Todo} alt="" />  To do  </h2>
-    </section>
-  )
-}
+const TaskColumn = ({ title, icon, tasks, status, handleDelete }) => {
+    return (
+        <section className='task_column'>
+            <h2 className='task_column_heading'>
+                <img className='task_column_icon' src={icon} alt='' /> {title}
+            </h2>
 
-export default TaskColumn
+            {tasks.map(
+                (task, index) =>
+                    task.status === status && (
+                        <TaskCard
+                            key={index}
+                            title={task.task}
+                            tags={task.tags}
+                            handleDelete={handleDelete}
+                            index={index}
+                        />
+                    )
+            )}
+        </section>
+    );
+};
+
+export default TaskColumn;
